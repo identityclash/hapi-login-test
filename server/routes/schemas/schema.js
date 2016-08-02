e072aea5ab9a75d5d6c5c2051bd911803fa6a17f
Jerome A. C. Ngo <jeromengo@gmail.com>
2016-08-02 11:46:53 +0800

    Transfer schema file to schemas folder

---
+++ server/routes/schemas/schema.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Omnius on 18/07/2016.
+ */
+'use strict';
+
+const Joi = require('joi');
+
+module.exports = {
+    username: Joi.string().required().min(3).max(20).regex(/^([a-zA-Z0-9]{3,20})$/),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8).max(20).regex(/^([a-zA-Z0-9!@#$%^&*()]{8,20})$/),
+    firstname: Joi.string().required().min(2).max(20).regex(/^([a-zA-Z0-9]{3,20})$/),
+    surname: Joi.string().required().min(2).max(20).regex(/^([a-zA-Z0-9]{3,20})$/),
+    birthdate: Joi.date().required().iso().format('YYYY-MM-DD').min('1900-01-01').max('2013-12-31'),
+    realm: Joi.string().regex(/^([a-zA-Z0-9@*#\s]{0,15})$/)
+};
