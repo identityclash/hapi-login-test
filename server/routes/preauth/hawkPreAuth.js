053583fd957bd6eb0a6ad41f3654fce9c4916224
Jerome A. C. Ngo <jeromengo@gmail.com>
2016-08-08 22:04:02 +0800

    Modify auth URL

--- server/routes/preauth/hawkPreAuth.js
+++ server/routes/preauth/hawkPreAuth.js
@@ -1,5 +1,12 @@
 /**
  * Created by Omnius on 18/07/2016.
+ *
+ * These codes were done to address the inability of the client web pages (non-framework-based) to set its own headers
+ * on a form GET request (NOT XHR requests), i.e. web page content instead of JSON. Eran Hammer's Hawk server codes
+ * (and hapi-auth-hawk) checks the client-sent headers for a MAC and a hash that was retrieved from the
+ * ID, key, and algorithm processing using the Hawk client.  This file acts as a middleware/adapter to convert the
+ * session cookie information into Hawk-readable header data for hapi-auth-hawk.
+ *
  */
 'use strict';
 
@@ -38,7 +45,7 @@ module.exports = (request, reply) => {
 
         const url = request.connection.info.protocol
             + '://'
-            + request.info.host + '/user/welcomeh';
+            + request.info.host + '/user/profile';
 
         request.raw.req.url = url;
 
