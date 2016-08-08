79e66778557fbcaa4055a01b92fde91ab0aca738
Jerome A. C. Ngo <jeromengo@gmail.com>
2016-08-08 22:12:24 +0800

    Submit username and password and receive response using XHR

--- views/js/helpers/login/loginsubmitter.js
+++ views/js/helpers/login/loginsubmitter.js
@@ -1,15 +1,28 @@
 /**
  * Created by Omnius on 6/27/16.
  */
-const submitForm = function () {
+const submitForm = () => {
 
     // TODO: Supposedly needs sanitation using ESAPI.encoder().encodeForJavascript() or some other sanitation
     // mechanism on inputUsername and inputPassword
     const username = $('#inputUsername').val();
     const password = $('#inputPassword').val();
 
-    // // Not the best way to send the username and password, but it will suffice for demo purposes for now.
-    $('#formLogin').attr('action', 'http://' + username + ':' + password + '@' + location.host + '/user/welcomeb');
-
-    $('#formLogin').submit();
+    $.ajax({
+        type: 'GET',
+        beforeSend: function (request) {
+            request.setRequestHeader('Authorization', 'Basic ' + btoa(username + ':' + password));
+        },
+        url: '/auth/basic',
+        processData: false,
+        success: function(data, textStatus, jQxhr){
+            $('#formLogin').attr('action', '/user/welcome');
+            $('#formLogin').submit();
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            if (xhr.status === 401) {
+                $('#formUnauthorized').removeClass('hidden');
+            }
+        }
+    });
 };
