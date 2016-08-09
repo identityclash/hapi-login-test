00a4a887f558b1775ea7311138c53ad29303edb2
Jerome A. C. Ngo <jeromengo@gmail.com>
2016-08-09 15:57:15 +0800

    Test port change for hapi-auth-hawk plugin

--- server/auth/hawk/hawkAuth.js
+++ server/auth/hawk/hawkAuth.js
@@ -36,6 +36,9 @@ exports.register = (server, options, next) => {
 
                 return callback(null, credentials);
             });
+        },
+        hawk: {
+            port: 443
         }
     });
 
