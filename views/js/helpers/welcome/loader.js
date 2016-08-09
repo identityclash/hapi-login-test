87b3fa4582e0c863247f4990c3d42272c92959ce
Jerome A. C. Ngo <jeromengo@gmail.com>
2016-08-09 15:56:35 +0800

    Remove extraneous code

--- views/js/helpers/welcome/loader.js
+++ views/js/helpers/welcome/loader.js
@@ -2,8 +2,6 @@
  * Created by Omnius on 6/27/16.
  */
 $(document).ready(function() {
-    const hawkSessionTokenCookie = MyUtils.getCookie('Hawk-Session-Token');
-
     $('#btnRetrieve').click(function () {
         retrieveUserInfo();
     });
