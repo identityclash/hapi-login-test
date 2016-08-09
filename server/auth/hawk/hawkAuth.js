67fa9ac54356b17db5b7e5b4d00a59ad5403386a
Jerome A. C. Ngo <jeromengo@gmail.com>
2016-08-09 16:23:27 +0800

    Test port config definition for hapi-auth-hawk

--- server/auth/hawk/hawkAuth.js
+++ server/auth/hawk/hawkAuth.js
@@ -38,7 +38,7 @@ exports.register = (server, options, next) => {
             });
         },
         hawk: {
-            port: 443
+            port: process.env.PORT || 8088
         }
     });
 
