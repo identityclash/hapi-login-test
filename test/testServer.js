136bbe9b5006068ee4141ba8313165759f3385c8
Jerome A. C. Ngo <jeromengo@gmail.com>
2016-08-23 11:12:04 +0800

    Remove hapi-ioredis from testServer

--- test/testServer.js
+++ test/testServer.js
@@ -7,7 +7,7 @@ const Hapi = require('hapi');
 const HapiAuthBasic = require('hapi-auth-basic');
 const HapiAuthHawk = require('hapi-auth-hawk');
 const Inert = require('inert');
-// const Redis = require('hapi-ioredis');
+
 
 module.exports = function () {
 
@@ -17,17 +17,20 @@ module.exports = function () {
         host: '127.0.0.1',
         port: '9001'
     });
+
+    server.state('Hawk-Session-Token', {
+        ttl: 24 * 60 * 60 * 1000,
+        path: '/',
+        isSecure: false,
+        isHttpOnly: false,
+        encoding: 'base64json',
+        clearInvalid: true
+    });
+
     server.register([
         Inert,
         HapiAuthBasic,
         HapiAuthHawk
-        // ,
-        // {
-        //     register: Redis,
-        //     options: {
-        //         url: 'redis://127.0.0.1:6379'
-        //     }
-        // }
     ], (err) => {
 
         if (err) {
