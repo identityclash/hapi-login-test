b7e1c80d4f736957fc2de28c7018ab08b2656870
Jerome A. C. Ngo <jeromengo@gmail.com>
2016-08-23 23:04:14 +0800

    Add reply.continue to allow progression

--- server/handlers/webHandler.js
+++ server/handlers/webHandler.js
@@ -11,10 +11,11 @@ module.exports = (route, options) => {
             return reply.view('registration')
                 .header('X-Permitted-Cross-Domain-Policies', 'master-only');
         }
-
         if (options.type === 'index') {
             return reply.view('login')
                 .header('X-Permitted-Cross-Domain-Policies', 'master-only');
         }
+
+        return reply.continue();
     };
 };
