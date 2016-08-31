191dabd1d1262c3f9e21fa41cd2e1b9ad5128b30
Jerome A. C. Ngo <jeromengo@gmail.com>
2016-09-01 00:29:47 +0800

    Add test to validate if values match hexadecimal characters

--- test/helpers/hkdfTokenGeneratorTest.js
+++ test/helpers/hkdfTokenGeneratorTest.js
@@ -17,11 +17,11 @@ describe('server/helpers/hkdfTokenGenerator', () => {
 
     it('generates a token', (done) => {
 
-        HkdfTokenGenerator.generateToken('127.0.0.1/hawkToken', null, (id, key, token) => {
+        HkdfTokenGenerator.generateToken('127.0.0.1/hawkSessionToken', null, (id, key, token) => {
 
-            expect(id).to.be.not.null();
-            expect(key).to.be.not.null();
-            expect(token).to.be.not.null();
+            expect(id).to.match(/^[a-fA-F0-9]+$/);
+            expect(key).to.match(/^[a-fA-F0-9]+$/);
+            expect(token).to.match(/^[a-fA-F0-9]+$/);
 
             return done();
         });
